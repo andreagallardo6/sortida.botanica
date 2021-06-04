629391e840fd1e1833f0e08e76d4cf5ab5e25192
andreagallardo6 <andrea.gallardo01@estudiant.upf.edu>
2021-06-04 22:58:36 +0200

    editat2

--- Excursió.R
+++ Excursió.R
@@ -6,14 +6,21 @@ dist2 <- factor(dist)
 
 dat1 <- data.frame(d = levels(dist2), r = c(11,12,9,10,6,7))
 
+par(mar=c(5,5,5,5))
 g <- plot(dat1,
      main = "Variació de la riquesa específica",
      xlab = "Distància al camí",
      ylab = "Riquesa específica",
+     xlim = c(-2,30),
      ylim = c(0,15),
      frame.plot = FALSE,
      las = 1,
-     n.breaks = 10)
+     pch = 16,
+     lwd = 3,
+     cex = 2,
+     col = 42,
+     type = "b",
+     fg = 48,
+     yaxs = "i",
+     pos = 0)
 
-
-?plot
